1c36afd321d28530fb5254218ef5b10a60d23032
Jstills60@team1225.com <OSXLion10!>
2020-10-18 21:54:07 -0400

    Restore
    Site Version 2.9.9

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,26 +272,3 @@ setInterval(function() {
 
 })(jQuery);
 
-// this one is jut to wait for the page to load
-document.addEventListener('DOMContentLoaded', () => {
-
-    const themeStylesheet = document.getElementById('theme');
-    const storedTheme = localStorage.getItem('theme');
-    if(storedTheme){
-        themeStylesheet.href = storedTheme;
-    }
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        // if it's light -> go dark
-        if(themeStylesheet.href.includes('light')){
-            themeStylesheet.href = '/assets/css/main-dark.css';
-            themeToggle.innerText = 'Switch to light mode';
-        } else {
-            // if it's dark -> go light
-            themeStylesheet.href = '/assets/css/main-light.css';
-            themeToggle.innerText = 'Switch to dark mode';
-        }
-        // save the preference to localStorage
-        localStorage.setItem('theme',themeStylesheet.href)  
-    })
-})
