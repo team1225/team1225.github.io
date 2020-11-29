61959c557c759382dcbaf9344a18a2845a8a93d7
thegorillas1225 <59852719+thegorillas1225@users.noreply.github.com>
2020-11-28 22:22:43 -0500

    Test Code

---
+++ fcf-assets/fcf.config.php
@@ -0,0 +1,120 @@
+<?php
+// ***********************************************************
+// This file is part of the Team 1225 Contact System:
+// www.team1225.com
+
+// Version 1.0
+
+// © 2020 by Henderson County Robotics Team 1225, The Gorillas
+// Created and Designed by Joey Stills
+// ***********************************************************
+
+// ***********
+// LICENSE KEY
+// ***********
+define('KEY', 'FREE');
+
+
+// *********************
+// FORM FIELD VALIDATION
+// *********************
+$rules = array(
+  "Name" => array(
+    "required" => true,
+    "label" => "Your name",
+    "maxLength" => 100
+  ),
+  "Email" => array(
+    "required" => true,
+    "label" => "Your email address",
+    "maxLength" => 100,
+    "email" => true
+  ),
+  "Phone" => array(
+    "required" => false,
+    "label" => "Your phone number",
+    "maxLength" => 30
+  ),
+  "Message" => array(
+    "required" => true,
+    "label" => "Your message",
+    "maxLength" => 3000
+  )
+);
+
+
+// ******************
+// THANK YOU PAGE
+// ******************
+define('THANK_YOU_PAGE','');
+
+
+// **************************
+// EMAIL TEMPLATES - INCOMING
+// **************************
+define('EMAIL_TEMPLATE_IN_HTML', 'fcf.email-in.htm');
+define('EMAIL_TEMPLATE_IN_TEXT', 'fcf.email-in.txt');
+
+
+// *******************************
+// EMAIL TEMPLATES - AUTO-RESPONSE
+// *******************************
+define('EMAIL_TEMPLATE_OUT_HTML', 'fcf.email-out.htm');
+define('EMAIL_TEMPLATE_OUT_TEXT', 'fcf.email-out.txt');
+
+define('SEND_AUTO_RESPONSE', 'NO'); // YES OR NO
+define('EMAIL_OUT_SUBJECT', '');
+define('EMAIL_OUT_TO', 'FIELD:Email');
+define('EMAIL_OUT_TO_NAME', 'FIELD:Name');
+define('EMAIL_OUT_FROM', '');
+define('EMAIL_OUT_FROM_NAME', '');
+
+
+// *************
+// EMAIL MESSAGE
+// *************
+define('EMAIL_TO', 'thegorillas@team1225.com');
+define('EMAIL_TO_NAME', 'Team 1225, The Gorillas');
+
+define('EMAIL_TO_CC', 'jstills60@team1225.com');
+define('EMAIL_TO_CC_NAME', 'Joey Stills');
+
+define('EMAIL_TO_BCC', '');
+define('EMAIL_TO_BCC_NAME', '');
+
+define('EMAIL_FROM', 'itadmin@team1225.com');
+define('EMAIL_FROM_NAME', 'Team 1225 Contact System');
+
+define('EMAIL_REPLY_TO', 'FIELD:Email');
+define('EMAIL_REPLY_TO_NAME', 'FIELD:Email');
+
+define('EMAIL_SUBJECT_BEFORE', '');
+define('EMAIL_SUBJECT', "Contact Form Message");
+define('EMAIL_SUBJECT_AFTER', '');
+
+
+
+// ***************
+// EMAIL TRANSPORT
+// ***************
+define('USE_SMTP', 'NO'); // YES or NO
+define('SMTP_HOST', '');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
+define('SMTP_AUTH', '');
+define('SMTP_SECURE', ''); // STARTTLS, SMTPS (port 465) or empty
+define('SMTP_PORT', '');
+define('SMTP_DEBUG', 'NO'); // YES or NO
+
+
+
+// **************************
+//    DON'T CHANGE BELOW
+// USED FOR VALIDATION CHECKS
+// **************************
+define('A', 'Rm9ybSBwcm92aWRlZCBieSB3d3cuZnJlZWNvbnRhY3Rmb3JtLmNvbQ==');
+define('B', 'Rm9ybSBwcm92aWRlZCBieSA8YSBocmVmPSJodHRwczovL3d3dy5mcmVlY29udGFjdGZvcm0uY29tIj5GcmVlQ29udGFjdEZvcm0uY29tPC9hPg==');
+define('C', 'Rm9ybSBwcm92aWRlZCBieSA8YSBocmVmPSJodHRwczovL3d3dy5mcmVlY29udGFjdGZvcm0uY29tIiB0YXJnZXQ9Il9ibGFuayI+RnJlZUNvbnRhY3RGb3JtLmNvbTwvYT4=');
+define('D', 'Y29uZ3JhdHVsYXRpb25zIGZvciBiZWluZyBjbGV2ZXIh');
+define('E', 'OGZlR3dSYkh3MjhGbg==');
+define('F', 'RlJFRQ==');
